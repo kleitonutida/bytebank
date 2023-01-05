87a30ea411ff4e0cdc839a53a347b224f949c89a
Kleiton Utida <kleiton.utida@mercadolivre.com>
2023-01-05 11:30:40 -0300

    Treating null and non-null references

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -3,7 +3,20 @@ package br.com.alura.bytebank
 import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
-    var enderecoNulo: Endereco? = null
-    val enderecoNaoNulo: Endereco = enderecoNulo!!
-    enderecoNaoNulo.logradouro
+    var enderecoNulo: Endereco? = Endereco(logradouro = "Rua Vergueiro", complemento = "Prédio")
+    var logradouro: String? = enderecoNulo?.logradouro
+    enderecoNulo?.let {
+        println(it.logradouro.length)
+        var tamanhoComplemento =
+            it.complemento?.length ?: throw IllegalArgumentException("Complemento não pode ser vazio")
+        println(tamanhoComplemento)
+    }
+
+    testeCastSafety(1)
+    testeCastSafety("")
+}
+
+fun testeCastSafety(valor: Any) {
+    var valorConvertido: Int? = valor as? Int
+    println(valorConvertido)
 }
