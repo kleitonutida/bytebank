72a5c2d6c66b26d3a24122097aa2d5d07afb275b
Kleiton Utida <kleiton.utida@mercadolivre.com>
2023-01-05 09:45:34 -0300

    Add class test testaExcecao

---
+++ src/main/kotlin/br/com/alura/bytebank/teste/testaExcecao.kt
@@ -0,0 +1,25 @@
+package br.com.alura.bytebank.teste
+
+fun testaExcecao() {
+    val entrada = "1.0"
+
+    val valorRecebido: Double? = try {
+        entrada.toDouble()
+    } catch (e: NumberFormatException) {
+        println("Problema na conversão")
+        e.printStackTrace()
+        null
+    }
+
+    val valorComTaxa: Double? = if (valorRecebido != null) {
+        valorRecebido + 0.1
+    } else {
+        null
+    }
+
+    if (valorComTaxa != null) {
+        println("Valor recebido: $valorComTaxa")
+    } else {
+        println("Valor inválido")
+    }
+}
